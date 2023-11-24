08eaf4ab5fa2682cb7d6ff30399658728dd96e63
Ricardo Victor <ricardovictor737@gmail.com>
2023-11-23 21:18:25 -0300

    Header add

--- src/pages/Home/home.tsx
+++ src/pages/Home/home.tsx
@@ -1,8 +1,12 @@
 import { AppBar, Container, Typography } from "@mui/material";
 import React from "react";
+import Header from "../../shared/components/header";
+
 
 function Home(){
-    return <h1>test</h1>
+    return(
+        <Header/>
+    )
 } 
 
 export default Home;
---
+++ src/shared/components/header.tsx
@@ -0,0 +1,25 @@
+import { AppBar, Button, CardMedia, Menu, Typography } from "@mui/material";
+import MenuBookIcon from '@mui/icons-material/MenuBook';
+import AddIcon from '@mui/icons-material/Add';
+
+import React from "react";
+import App from "../../pages/App/App";
+
+function Header(){
+    return (
+        <AppBar sx={{bgcolor: '#3F71D3', height:'65px', display: 'flex', flexDirection:'row', alignItems:'center'}}>
+            <Button sx={{color:'black', width:'200px', height:'45px', bgcolor: 'white', borderRadius:'10px', marginLeft: '60px', gap: '20px', display:'flex', justifyContent:'center', alignItems: 'center'}}>
+              <MenuBookIcon fontSize="large"/>
+              <Typography sx={{fontWeight:'bold'}}>Quests</Typography> 
+            </Button>
+
+            <Button sx={{color:'black', width:'200px', height:'45px', bgcolor: 'white', borderRadius:'10px', marginLeft: '70px', gap: '20px', display:'flex', justifyContent:'space-between', alignItems: 'center'}}>
+                <AddIcon fontSize="large"/>
+                <Typography sx={{}}>Criar card</Typography>
+            </Button>
+
+        </AppBar>
+    );
+}
+
+export default Header;
