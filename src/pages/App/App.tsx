9232872139710ae323fc0c335dbdc035bb49c899
Kinderman <kindbarros@gmail.com>
2023-11-24 14:06:32 -0400

    Changed landing pages

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -4,29 +4,23 @@ import logo from "../../assets/icons/logo.svg";
 import { Link } from "react-router-dom";
 import Card from "../../shared/components/card/Card";
 
-
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
+        <p />
         <Link to="/signin">
           <img src={logo} className="App-logo" alt="logo" />
         </Link>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Sidequest é uma plataforma para utilização de quests em sala de aula.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <div>
+        <p>Dever de casa nunca foi tão divertido!</p>
+      
+        <div>
         <Card title="My Card Title" buttonText="Click me" />
       </div>
+      </header>
     </div>
   );
 };
