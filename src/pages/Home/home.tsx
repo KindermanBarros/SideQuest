0af804462a73bb392558fde6eec271bf26472485
Ricardo Victor <ricardovictor737@gmail.com>
2023-11-23 22:34:59 -0300

    building Home

--- src/pages/Home/home.tsx
+++ src/pages/Home/home.tsx
@@ -1,11 +1,25 @@
-import { AppBar, Container, Typography } from "@mui/material";
+import { AppBar, Box, Container, Typography } from "@mui/material";
 import React from "react";
 import Header from "../../shared/header/header";
 
 
 function Home(){
     return(
-        <Header/>
+        <Box sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            height: '100vh',
+            bgcolor: '#05445E'
+        }}>
+            <Header/>
+            
+            <Typography sx={{fontWeight: 'bold'}}>Ranking</Typography>
+
+            <Box>
+
+            </Box>
+        </Box>
+        
     )
 } 
 
